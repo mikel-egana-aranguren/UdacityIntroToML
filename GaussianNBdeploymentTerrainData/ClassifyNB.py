fea5221b5779de3804584d5afc6ffe9a920006a5
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2018-10-23 12:25:09 +0200

    GaussianNB Deployment on Terrain Data Accuracy

--- GaussianNBdeploymentTerrainData/ClassifyNB.py
+++ GaussianNBdeploymentTerrainData/ClassifyNB.py
@@ -1,15 +1,9 @@
 def classify(features_train, labels_train):   
     ### import the sklearn module for GaussianNB
-    ### create classifier
-    ### fit the classifier on the training features and labels
-    ### return the fit classifier
-    
-    
-    ### your code goes here!
-    
-    
     from sklearn.naive_bayes import GaussianNB
+    ### create classifier
     clf = GaussianNB()
+    ### fit the classifier on the training features and labels
     clf.fit(features_train, labels_train)
-    #pred = clf.predict(features_test)
+    ### return the fit classifier
     return clf
